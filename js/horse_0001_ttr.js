ba58cd69c9c6854c1443376db41c9c399254951c
ike <ike0738@ikeda-naobun-no-MacBook-Pro-10.local>
2023-02-13 21:03:09 +0900

    更新履歴追加。馬たちの体調ページ開設。トップ画像追加

---
+++ js/horse_0001_ttr.js
@@ -0,0 +1,38 @@
+var ctx = document.getElementById('horse_0001_ttr').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['2023.02.08','2023.02.09','2023.02.10','2023.02.11','2023.02.12','2023.02.13',''],
+    datasets: [{
+      label: '体重',
+      data: [402,397,399,395,398,397,],
+      borderColor: '#333',
+    }, {
+      label: '体温',
+      data: [37.5, 37.3, 37.3, 37.1, 37.3, 37.8, ],
+      borderColor: '#f88',
+      yAxisID: 'y2',
+    }],
+  },
+  options: {
+    scales: {
+      y: {   // 体重
+        min: 350,
+        max: 550,
+        ticks: {
+          color: '#333',
+          stepSize: 25,
+        },
+      },
+      y2: { // 体温
+        min: 35,
+        max: 45,
+        position: 'right',
+        ticks: {
+          color: '#f88',
+          stepSize: 2.5,
+        },
+      },
+    },
+  },
+});
---
+++ js/horse_0002_glm.js
@@ -0,0 +1,38 @@
+var ctx = document.getElementById('horse_0002_glm').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['2023.02.08','2023.02.09','2023.02.10','2023.02.11','2023.02.12','2023.02.13',''],
+    datasets: [{
+      label: '体重',
+      data: [478,478,478,477,481,483,],
+      borderColor: '#333',
+    }, {
+      label: '体温',
+      data: [37.3, 37.3, 37.4, 37.2, 37.4, 37.4, ],
+      borderColor: '#f88',
+      yAxisID: 'y2',
+    }],
+  },
+  options: {
+    scales: {
+      y: {   // 体重
+        min: 350,
+        max: 550,
+        ticks: {
+          color: '#333',
+          stepSize: 25,
+        },
+      },
+      y2: { // 体温
+        min: 35,
+        max: 45,
+        position: 'right',
+        ticks: {
+          color: '#f88',
+          stepSize: 2.5,
+        },
+      },
+    },
+  },
+});
